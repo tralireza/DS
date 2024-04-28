957d3ef7e21636b8c356dadd305b4138e9d1fb48
Alireza <26859725+tralireza@users.noreply.github.com>
2024-04-28 20:53:13 +0100

    DS

---
+++ ds_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("")
+
+	log.Print("> DS")
+}
+
+// 834h Sum of Distances in Tree
+func Test834(t *testing.T) {
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module DS
+
+go 1.22.2
