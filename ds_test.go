50dab41889bdcae84524a2398a0158e6e9afff65
Alireza <26859725+tralireza@users.noreply.github.com>
2024-04-29 03:48:01 +0100

    DP, DFS 834h Sum of Distances in Tree: dfs on root, then reroot

--- ds_test.go
+++ ds_test.go
@@ -14,4 +14,46 @@ func init() {
 
 // 834h Sum of Distances in Tree
 func Test834(t *testing.T) {
+	sumOfDistancesInTree := func(n int, edges [][]int) []int {
+		lsAdj := make([][]int, n)
+
+		for _, edge := range edges {
+			v, u := edge[0], edge[1]
+			lsAdj[v] = append(lsAdj[v], u)
+			lsAdj[u] = append(lsAdj[u], v)
+		}
+
+		dists, subTreeNds := make([]int, n), make([]int, n)
+
+		var walk func(v, p int)
+		walk = func(v, p int) {
+			subTreeNds[v] = 1
+			for _, u := range lsAdj[v] {
+				if u != p {
+					walk(u, v)
+					subTreeNds[v] += subTreeNds[u]
+					dists[v] += dists[u] + subTreeNds[u]
+				}
+			}
+		}
+		walk(0, -1)
+		log.Print(dists, subTreeNds)
+
+		var reRoot func(v, p int)
+		reRoot = func(v, p int) {
+			for _, u := range lsAdj[v] {
+				if u != p {
+					dists[u] = (dists[v] - subTreeNds[u]) + (n - subTreeNds[u])
+					reRoot(u, v)
+				}
+			}
+		}
+		reRoot(0, -1)
+		log.Print(dists, subTreeNds)
+
+		return dists
+	}
+
+	log.Print("[8 12 6 10 10 10] ?= ", sumOfDistancesInTree(6, [][]int{{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}}))
+	log.Print("[1 1] ?= ", sumOfDistancesInTree(2, [][]int{{0, 1}}))
 }
