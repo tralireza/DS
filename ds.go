cd655369cb089b1fc1f6c75e4e45eedd4d660b13
Alireza <26859725+tralireza@users.noreply.github.com>
2024-05-17 22:17:59 +0100

    Binary Tree 1325m Delete Leaves With a Given Value -> PostOrder walk

--- ds.go
+++ ds.go
@@ -311,3 +311,17 @@ func evaluateTree(root *TreeNode) bool {
 	}
 	return l && r
 }
+
+// 1325m Delete Leaves With a Given Value
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left = removeLeafNodes(root.Left, target)
+	root.Right = removeLeafNodes(root.Right, target)
+	if root.Left == nil && root.Right == nil && root.Val == target {
+		return nil
+	}
+	return root
+}
--- ds_test.go
+++ ds_test.go
@@ -512,3 +512,30 @@ func Test2331(t *testing.T) {
 		log.Print("false ?= ", f(&T{Val: 0}))
 	}
 }
+
+// 1325m Delete Leaves With a Given Value
+func Test1325(t *testing.T) {
+	type T = TreeNode
+
+	var Draw func(n *TreeNode)
+	Draw = func(n *TreeNode) {
+		if n != nil {
+			Draw(n.Left)
+			l, r := '-', '-'
+			if n.Left != nil {
+				l = '*'
+			}
+			if n.Right != nil {
+				r = '*'
+			}
+			fmt.Printf("{%c %d %c}", l, n.Val, r)
+			Draw(n.Right)
+		}
+	}
+
+	r := &T{1, &T{2, &T{Val: 2}, nil}, &T{3, &T{Val: 2}, &T{Val: 4}}}
+	Draw(r)
+	fmt.Print(" -> ")
+	Draw(removeLeafNodes(r, 2))
+	fmt.Println()
+}
